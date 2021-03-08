1de73c156fce4ab66beca4df90acea5093acf392
ataqi23 <alif.taqi00@gmail.com>
2021-03-07 21:54:01 -0800

    Update refactor

--- pull.sh
+++ pull.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Remove the existing files
-rm -rf R/eigen.R
-#rm -rf R/ensemble.R
 rm -rf R/matrices.R
-rm -rf R/matrices-diagnostics.R
+rm -rf R/spectrum.R
+rm -rf R/dispersion.R
+rm -rf R/mat-diag.R
 
 # Copy them from developing environment
-cp ../thesis-taqi/R/eigen.R R/eigen.R
-#cp ../thesis-taqi/R/ensemble.R R/ensemble.R
 cp ../thesis-taqi/R/matrices.R R/matrices.R
+cp ../thesis-taqi/R/spectrum.R R/spectrum.R
+cp ../thesis-taqi/R/dispersion.R R/dispersion.R
 cp ../thesis-taqi/R/mat-diag.R R/mat-diag.R
+
 
