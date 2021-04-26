477a8aafcca586c41b0b8296f13e70cf4fc10e03
Ali Taqi <alif.taqi00@gmail.com>
2021-04-25 20:02:49 -0700

    Minimal, fully working package

--- pull.sh
+++ pull.sh
@@ -4,7 +4,8 @@
 rm -rf R/matrices.R
 rm -rf R/spectrum.R
 rm -rf R/dispersion.R
-rm -rf R/plot.R
+
+#rm -rf R/plot.R
 #rm -rf R/parallel.R
 #rm -rf R/mat-diag.R
 
@@ -12,7 +13,8 @@ rm -rf R/plot.R
 cp ../thesis-taqi/R/matrices.R R/matrices.R
 cp ../thesis-taqi/R/spectrum.R R/spectrum.R
 cp ../thesis-taqi/R/dispersion.R R/dispersion.R
-cp ../thesis-taqi/R/plot.R R/plot.R
+
+#cp ../thesis-taqi/R/plot.R R/plot.R
 #cp ../thesis-taqi/R/parallel.R R/parallel.R
 #cp ../thesis-taqi/R/mat-diag.R R/mat-diag.R
 
